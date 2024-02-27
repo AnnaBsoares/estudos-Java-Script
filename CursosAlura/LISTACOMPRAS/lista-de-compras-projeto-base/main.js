c5b1127b10f421ea652c7dde2448eb14d0b80174
Anna <rm99814@fiap.com.br>
2024-02-27 16:13:04 -0300

    vai

---
+++ CursosAlura/LISTACOMPRAS/lista-de-compras-projeto-base/main.js
@@ -0,0 +1,13 @@
+let listaCompra=['Arroz', 'Biscoito','Suco  ']
+
+constlistadeItens={
+    item1:'bolacha',
+    item2:'Suco',
+    quantidade1:3,
+    quantidade2:4,
+    mostrarItens:function(){
+        alert('Comprei'+listadeItens.quantidade1+'pacotes de'+listadeItens.item1)
+    }
+
+}
+listadeItens. mostrarItens()
---
+++ CursosAlura/devsedragons/index.js
@@ -0,0 +1,32 @@
+
+const personagemjose ={
+    nome: 'José',
+    vida: 7,
+    mana:6,
+    level:3,
+    tipo:'arqueiro',
+}
+
+
+const personagemAnna={
+    nome: 'Anna',
+    vida:8,
+    mana:10,
+
+}
+class Personagem{
+    nome
+    vida 
+    mana
+    level 
+    tipo 
+    descricao
+}
+
+const personagemPedrinho =new Personagem()
+personagemPedrinho.nome='Pedrinho'
+personagemPedrinho.mana=12
+personagemPedrinho.vida=7
+personagemPedrinho.level=
+personagemPedrinho.tipo=
+personagemPedrinho.descricao
