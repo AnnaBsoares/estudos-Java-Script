fae96e293cd2c714172da68e64cc217680cdb57c
Anna <rm99814@fiap.com.br>
2024-02-28 11:34:08 -0300

    atu

--- CursosAlura/LISTACOMPRAS/lista-de-compras-projeto-base/main.js
+++ CursosAlura/LISTACOMPRAS/lista-de-compras-projeto-base/main.js
@@ -1,13 +1,2 @@
-let listaCompra=['Arroz', 'Biscoito','Suco  ']
 
-constlistadeItens={
-    item1:'bolacha',
-    item2:'Suco',
-    quantidade1:3,
-    quantidade2:4,
-    mostrarItens:function(){
-        alert('Comprei'+listadeItens.quantidade1+'pacotes de'+listadeItens.item1)
-    }
-
-}
-listadeItens. mostrarItens()
+const listadeItens= []
